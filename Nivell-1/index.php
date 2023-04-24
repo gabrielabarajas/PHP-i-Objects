72aa29e1d042a05e4a4e785158236ae02d61cba0
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2023-04-24 11:13:59 +0200

    Excercici 2 done and tested

--- Nivell-1/index.php
+++ Nivell-1/index.php
@@ -1,6 +1,6 @@
 <?php
 
-//Ejercicio 1
+//Exercici 1
 class Employee{
 private $name;
 private $salary;
@@ -19,7 +19,7 @@ public function print(){
 }
 }
 
-//Testing Ejercicio 1
+//Testing Exercici 1
 
 $employeeOne = new Employee("Gabriela", 6500);
 echo $employeeOne->print();
@@ -31,6 +31,45 @@ echo "</br>";
 
 $employeeThree = new Employee("Thiago", 500);
 echo $employeeThree->print();
+echo "</br>";
 
+//Exercici 2
+class Shape{
+    public $width;
+    public $height;
+
+    public function __construct($width,$height){
+        $this->width= $width;
+        $this->height= $height;
+    }
+}
+
+class Triangle extends Shape{
+    private $area;
+
+    public function area(){
+        echo $this->area = ($this->width * $this->height)/2;
+    }
+}
+
+class Rectangle extends Shape{
+    private $area;
+
+    public function area(){
+        echo $this->area = ($this->width * $this->height);
+    }
+}
+
+//Testing Exercici 2
+
+$triangleOne = new Triangle(10, 20);
+echo "El área del Triángulo 1 es:  ";
+$triangleOne->area();
+echo "</br>";
+
+$rectangleOne = new Rectangle(20, 10);
+echo "El área del Rectángulo 1 es:  ";
+$rectangleOne->area();
+echo "</br>";
 
 ?>
