1dd2206ebc4d60634611350aa166ffdc6128aca2
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2023-04-24 10:42:12 +0200

    Excercici 1 completed and tested

--- Nivell-1/index.php
+++ Nivell-1/index.php
@@ -5,17 +5,16 @@ class Employee{
 private $name;
 private $salary;
 
-private function __construct($name,$salary){
+public function __construct($name,$salary){
     $this->name= $name;
     $this->salary= $salary;
 }
 
 public function print(){
-
     if ($this->salary > 6000){
-        echo $this->name." - Ha de pagar impuestos -";
+        echo $this->name." - Ha de pagar impuestos.";
     }else{
-        echo $this->name." - No ha de pagar impuestos -";
+        echo $this->name." - No ha de pagar impuestos.";
     };
 }
 }
@@ -23,7 +22,15 @@ public function print(){
 //Testing Ejercicio 1
 
 $employeeOne = new Employee("Gabriela", 6500);
+echo $employeeOne->print();
+echo "</br>";
+
 $employeeTwo = new Employee("Julio", 6000);
+echo $employeeTwo->print();
+echo "</br>";
+
 $employeeThree = new Employee("Thiago", 500);
+echo $employeeThree->print();
+
 
 ?>
